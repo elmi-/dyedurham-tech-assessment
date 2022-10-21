03ea95da3d85e3ce8a83a77b814b4d1659dd960a
Abdi <abdi.elmi@rocketmail.com>
2022-10-21 10:25:48 -0400

    fixed DetailsView.vue and ListViewDetails.vue issue

---
+++ src/components/ListViewDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <h4>{{ $store.state.filterValue }} details</h4>
+  <section
+    v-for="(value, key, index) in getDataByID($store.state.detailsValue, $store.state.filterValue)"
+    :key="index"
+  >
+    <article
+    v-for="(v, k, i) in value"
+    :key="i"
+    >
+      <strong>{{ k }}</strong>: <span v-show="k === 'total'">$</span>{{ v }}
+  </article>
+  </section>
+</template>
+<script>
+  import { mapGetters } from "vuex"
+  export default {
+    name: "ListViewDetails",
+    computed: {
+      ...mapGetters([
+        'data',
+        'filteredData',
+        'getDataByID'
+    ])
+  },
+  props: {
+      data: Object
+    },
+  } 
+</script>
--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -1,30 +1,12 @@
 <template>
-  <h4>{{ $store.state.filterValue }} details</h4>
-  <section
-    v-for="(value, key, index) in getDataByID($store.state.detailsValue, $store.state.filterValue)"
-    :key="index"
-  >
-    <article
-    v-for="(v, k, i) in value"
-    :key="i"
-    >
-      <strong>{{ k }}</strong>: <span v-show="k === 'total'">$</span>{{ v }}
-  </article>
-  </section>
+  <ListViewDetails />
 </template>
 <script>
-  import { mapGetters } from "vuex"
+  import ListViewDetails from '@/components/ListViewDetails.vue'
   export default {
-    name: "DetailsView",
-    computed: {
-      ...mapGetters([
-        'data',
-        'filteredData',
-        'getDataByID'
-    ])
-  },
-  props: {
-      data: Object
-    },
-  } 
+    name: "ListView",
+    components: {
+      ListViewDetails,
+    }
+  }
 </script>
